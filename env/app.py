2c691c0aa339814529d571f3d1ea8c61cb88304e
Richard Andrew Long <id-dev@Richards-MacBook-Air.local>
2024-01-25 20:18:23 -0600

    finish

---
+++ env/app.py
@@ -0,0 +1,90 @@
+# source .bin/activate - will activate the environment
+# deactivate - will close the environment
+
+import customtkinter as ctk
+from tkinter import ttk
+from pytube import YouTube
+import os
+
+def download_video():
+        url = entry_url.get()
+        resolution = resolution_var.get()
+        
+        progress_label.pack(pady=("10p", "5p"))
+        progress_bar.pack(pady=("10p", "5p"))
+        status_label.pack(pady=("10p", "5p"))
+        
+        try:
+                yt=YouTube(url, on_progress_callback=on_progress)
+                stream = yt.streams.filter(res=resolution).first()
+                
+                # download video to specific directory
+                os.path.join("downloads", f"{yt.title}.mp4")
+                stream.download(output_path="downloads")
+                
+                status_label.configure(text="Downloaded!", text_color="white", fg_color="green")
+                
+                
+        except Exception as e:
+                status_label.configure(text=f"Error {str(e)}", text_color="white", fg_color="red")
+        
+
+def on_progress(stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        percentage_completed  = bytes_downloaded / total_size * 100
+        
+        progress_label.configure(text= str(int(percentage_completed)) + "%")
+        progress_label.update()
+        
+        progress_bar.set(float(percentage_completed / 100))
+  
+#create a root window
+root = ctk.CTk()
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("green")
+
+# Title of the window
+root.title("YouTube Downloader")
+
+# set minimum and maximum width x height
+root.geometry("720x480")
+root.minsize(720, 480)
+root.maxsize(1080, 720)
+
+# create a frame to hold content
+content_frame = ctk.CTkFrame(root)
+content_frame.pack(fill=ctk.BOTH, expand=True, padx=10, pady=10)
+
+# create label and entry widget for the URL
+url_label = ctk.CTkLabel(content_frame, text="Enter the YouTube URL: ")
+entry_url = ctk.CTkEntry(content_frame, width=400, height=40)
+url_label.pack(pady=("10p", "5p"))
+entry_url.pack(pady=("10p", "5p"))
+
+#create a download button
+download_button = ctk.CTkButton(content_frame, text="Download", command=download_video)
+download_button.pack(pady=("10p", "5p"))
+
+# Choose resolution
+resolutions = ["1080p", "720p", "360p", "240p"]
+resolution_var = ctk.StringVar()
+resolution_combobox = ttk.Combobox(content_frame, values=resolutions, textvariable=resolution_var )
+resolution_combobox.pack(pady=("10p", "5p"))
+resolution_combobox.set("720p")
+
+# create label and progress bar
+progress_label = ctk.CTkLabel(content_frame, text="0%")
+# progress_label.pack(pady=("10p", "5p"))
+
+progress_bar = ctk.CTkProgressBar(content_frame, width=400)
+progress_bar.set(0)
+# progress_bar.pack(pady=("10p", "5p"))
+
+# Status Label
+status_label = ctk.CTkLabel(content_frame, text="")
+# status_label.pack(pady=("10p", "5p"))
+
+
+# to start app
+root.mainloop()
